093fe1ae1eeaf84f6f2e9edf2728f5d64ee904ba
3axap2509 <zakayshka2@gmail.com>
2019-02-08 18:25:52 +0300

    Add files via upload

---
+++ ResolutionChanger/Classes/DisplayFormat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolutionChanger.Classes
+{
+    public class DisplayFormat
+    {
+        public DisplayFormat(byte NW, byte NH)
+        {
+            this.NWidth = NW;
+            this.NHeight = NH;
+        }
+        public byte NWidth { get; set; }
+        public byte NHeight { get; set; }
+
+        public ushort HfromW(ushort Width)
+        {
+            return (ushort)(Width / NWidth * NHeight);
+        }
+        public ushort WfromH(ushort Height)
+        {
+            return (ushort)(Height / NHeight * NWidth);
+        }
+    }
+}
---
+++ ResolutionChanger/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using ResolutionChanger.Classes;
+
+namespace ResolutionChanger
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string BaseFolderPath;
+        private string SaveFolderPath;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BaseFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if(fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                BaseFolderTextBox.Text = fbd.SelectedPath;
+            }
+            fbd.Dispose();
+        }
+
+        private void SaveFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                SaveFolderTextBox.Text = fbd.SelectedPath;
+            }
+            fbd.Dispose();
+        }
+
+        private void BaseFolderTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BaseFolderPath = (sender as System.Windows.Controls.TextBox).Text;
+            StartEditButton.IsEnabled = (BaseFolderPath?.Length > 0 && SaveFolderPath?.Length > 0) ? true : false;
+        }
+
+        private void SaveFolderTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SaveFolderPath = (sender as System.Windows.Controls.TextBox).Text;
+            StartEditButton.IsEnabled =(BaseFolderPath?.Length > 0 && SaveFolderPath?.Length > 0) ? true : false;
+        }
+        private bool isComboBoxTextCorrect()
+        {
+            string buf = DisplayFormatComboBox.Text;
+            ushort a;
+            return buf.Contains(':') && UInt16.TryParse(buf.Split(':')[0], out a) && UInt16.TryParse(buf.Split(':')[1], out a);
+        }
+    }
+}
